6f2d4ecaa294bb343a3f82d308fbc7a0c51f4e1e
Balaks0702 <gayathriks0702@gmail.com>
2025-02-09 10:26:12 +0530

    add, sub, multiply, divide works; results in formatted answer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, {useState} from "react";
 import Wrapper from './components/Wrapper';
 import Screen from './components/Screen';
 import BtnBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
+//Button Values
 const btnVal = [
   ["C", "+-", "%", "/"],
   [7, 8, 9, "X"],
@@ -11,20 +13,105 @@ const btnVal = [
   [0, ".", "="],
 ];
 
-function App() {
+//result formating
+const toLocaleString = (num) => String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+const App = () => {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0, //selected val
+    res: 0, //result
+  });
+
+  //Number clicked
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num: calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  //add,sub,multiply,divide
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  //result handled
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Can't divide with 0"
+            : toLocaleString(
+                math(
+                  Number(removeSpaces(calc.res)),
+                  Number(removeSpaces(calc.num)),
+                  calc.sign
+                )
+              ),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+
+  //reset screen
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
 
       <BtnBox>
         {
           btnVal.flat().map((btn, i) => {
             return (
-              <Button key={i}
-                className={btn === "=" ? "equals" : ""}
-                value={btn} onClick={() => {
-                  console.log(`${btn} clicked!`);
-                }}
+              <Button key={i} className={btn === "=" ? "equals" : ""}
+                value={btn} onClick={btn === "C"
+                  ? resetClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : numClickHandler
+                }
               />
             );
           })
